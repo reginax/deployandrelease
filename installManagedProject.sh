a002bb7f60b9c09edb0ab2456adec032831b8e43
John Lowe <jblowe@berkeley.edu>
2015-06-05 13:31:22 -0700

    DJAN-4: more adjustments for managed servers

--- installManagedProject.sh
+++ installManagedProject.sh
@@ -7,7 +7,7 @@ if [ $# -ne 3 ]; then
     echo
     echo "e.g. sudo ./installProject.sh pahma pahmaNV pahma_4.0_1 > install.log 2>&1"
     echo
-    echo "this will install pahma_project branch pahma_4.0-1 from github.com/cspace-deployment"
+    echo "this will install pahma_project repo, branch pahma_4.0-1, from github.com/cspace-deployment"
     echo "into /var/www/pahmaNV and configure it to run, except for Apache conf.d"
     echo "================================================================================================="
     exit
@@ -20,9 +20,15 @@ if [ -e "$2" ]; then
     exit
 fi
 
-git clone https://github.com/cspace-deployment/$1_project /var/www/$2
+# start at home
+cd ~
+# clean out what was there, if anything
+rm -rf /var/www/$2
+mkdir /var/www/$2
+ln -s /var/www/$2 $2
+cd /var/www/$2
+git clone https://github.com/cspace-deployment/$1_project .
 
-cd /var/www/$2/
 git pull origin $3 -v
 git checkout $3
 
@@ -34,6 +40,7 @@ cp */*.cfg config/
 cp */*.csv config/
 cd config
 rm authn.cfg main.cfg
+# get rid of the old names, if there are any
 perl -i -pe 's/${t}_project/${t}/g' *
 
 cd
@@ -41,14 +48,14 @@ cat >> wsgi.conf <<EOM
 
 # $2
    
-Alias /$2_project_static ${PWD}/$2/static_root
+Alias /$2_static ${PWD}/$2/static_root
 
-WSGIDaemonProcess $2_wsgi python-path=${PWD}/$2:${PWD}/venv/lib/python2.6/site-packages user=app_pahma group=app_pahma
+WSGIDaemonProcess $2_wsgi python-path=/var/www/$2:${PWD}/venv/lib/python2.6/site-packages user=app_pahma group=app_pahma
 WSGIProcessGroup $2_wsgi
 
-WSGIScriptAlias /$2 ${PWD}/$2/cspace_django_site/wsgi.py process-group=$2_wsgi
+WSGIScriptAlias /$2 $/var/www/$2/cspace_django_site/wsgi.py process-group=$2_wsgi
 
-<Directory "${PWD}/$2">
+<Directory "/var/www/$2">
     Order deny,allow
     Allow from all
 </Directory>
